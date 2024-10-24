1f4ce4a16228298c8cc0daf9532f1f3a527161e3
Iaroslav <yar.bezruchko@gmail.com>
2024-10-24 14:56:02 +0300

    message updating props test

--- src/component.worker.tsx
+++ src/component.worker.tsx
@@ -1,22 +1,31 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 import { expose } from "react-worker-components";
-
 import RemoteComponent from "./RemoteComponent";
 
-onmessage = function (e) {
-  console.log("Message received from main script: " + JSON.stringify(e.data));
-  postMessage("Remote Worker response: " + Math.random());
-};
-
 const TextComponent = ({ text, children }: { text: string; children: any }) => {
+  const [updatedText, setUpdatedText] = useState(text);
+
+  useEffect(() => {
+    onmessage = function (e) {
+      console.log(
+        "Message received from main script: " + JSON.stringify(e.data)
+      );
+
+      // Update the component state with the received data
+      if (e.data && typeof e.data === "string") {
+        setUpdatedText(e.data);
+      }
+      postMessage("Remote Worker response: " + Math.random());
+    };
+  }, []);
+
   return (
     <div>
       <div>Hello from worker: {Math.random()}</div>
       <h1>Main TextBox</h1>
       {children}
       <h1>Worker TextBox</h1>
-      <RemoteComponent textProp={text} />
+      <RemoteComponent textProp={updatedText} />
     </div>
   );
 };
