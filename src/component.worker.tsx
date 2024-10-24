95e6b5e670e6cf524b40d18966022fc4bd4123bf
Iaroslav <yar.bezruchko@gmail.com>
2024-10-24 14:02:35 +0300

    children from main thread

--- src/component.worker.tsx
+++ src/component.worker.tsx
@@ -4,11 +4,12 @@ import { expose } from "react-worker-components";
 
 import RemoteComponent from "./RemoteComponent";
 
-const TextComponent = ({ text }: { text: string }) => {
+const TextComponent = ({ text, children }: { text: string; children: any }) => {
   return (
     <div>
       <div>Hello from worker: {Math.random()}</div>
       <h1>Main TextBox</h1>
+      {children}
       <h1>Worker TextBox</h1>
       <RemoteComponent textProp={text} />
     </div>
