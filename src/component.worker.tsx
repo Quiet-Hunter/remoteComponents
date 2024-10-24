f2c46c8afbb1404591022b1cbf203298d1b00ca1
Iaroslav <yar.bezruchko@gmail.com>
2024-10-24 15:44:15 +0300

    fix

--- src/component.worker.tsx
+++ src/component.worker.tsx
@@ -1,15 +1,8 @@
 import React from "react";
 
-import { expose, register } from "react-worker-components";
+import { expose } from "react-worker-components";
 
-const RemoteComponent: React.FC<{ textProp: string }> = ({ textProp }) => (
-  <>
-    <div>RemoteComponent.tsx</div>
-    <div>Props: {textProp}</div>
-  </>
-);
-
-register(RemoteComponent, "RemoteComponent");
+import RemoteComponent from "./RemoteComponent";
 
 onmessage = function (e) {
   console.log("Message received from main script: " + JSON.stringify(e.data));
