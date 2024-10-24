82c80f40ba7063a3c7e0ebeccf755428c5b15c9f
Iaroslav <yar.bezruchko@gmail.com>
2024-10-24 15:44:48 +0300

    fix

--- src/RemoteComponent.tsx
+++ src/RemoteComponent.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { register } from "react-worker-components";
 
-const RemoteComponent: React.FC<{ textProp: string }> = ({ textProp }) => (
+const RemoteComponent: React.FC<any> = (props) => (
   <>
     <div>RemoteComponent.tsx</div>
-    <div>Props: {textProp}</div>
+    <div>Props: {JSON.stringify(props)}</div>
   </>
 );
 
