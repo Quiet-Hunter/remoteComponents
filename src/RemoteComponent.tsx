5ebc981b0bd347a82d671b34fdcb47954173471e
Iaroslav <yar.bezruchko@gmail.com>
2024-10-29 16:21:32 +0200

    test

--- src/RemoteComponent.tsx
+++ src/RemoteComponent.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { register } from "react-worker-components";
+import { EventEmitter } from "events";
 
-const RemoteComponent: React.FC<any> = (props) => (
-  <>
-    <div>Remote RemoteComponent.tsx</div>
-    <div>Props: {JSON.stringify(props)}</div>
-  </>
-);
+const eventEmitter = new EventEmitter();
+
+const RemoteComponent: React.FC<any> = (props) => {
+  useEffect(() => {
+    const handleUpdateProps = (newProps: any) => {
+      console.log("RemoteComponent: " + newProps);
+    };
+
+    eventEmitter.on("updateProps", handleUpdateProps);
+    return () => {
+      eventEmitter.off("updateProps", handleUpdateProps);
+    };
+  }, []);
+
+  return (
+    <>
+      <div>Remote RemoteComponent.tsx</div>
+      <div>Props: {JSON.stringify(props)}</div>
+    </>
+  );
+};
 
 register(RemoteComponent, "RemoteComponent");
 
