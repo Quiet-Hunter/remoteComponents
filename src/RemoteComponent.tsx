9795fed8ff40bdce197e92bce3e88b5365f21b53
Iaroslav <yar.bezruchko@gmail.com>
2024-10-29 16:32:32 +0200

    test

--- src/RemoteComponent.tsx
+++ src/RemoteComponent.tsx
@@ -4,18 +4,16 @@ import { EventEmitter } from "events";
 
 const eventEmitter = new EventEmitter();
 
+self.onmessage = (event) => {
+  console.log(
+    "Worker received message (props from main thread): ",
+    event.data.props.o
+  );
+  eventEmitter.emit("updateProps", event.data.props.o);
+};
+
 const RemoteComponent: React.FC<any> = (props) => {
   const [componentProps, setComponentProps] = React.useState(props);
-  useEffect(() => {
-    self.onmessage = (event) => {
-      console.log(
-        "Worker received message (props from main thread): ",
-        event.data.props.o
-      );
-      eventEmitter.emit("updateProps", event.data.props.o);
-    };
-  }, []);
-
   useEffect(() => {
     const handleUpdateProps = (newProps: any) => {
       console.log("RemoteComponent: " + newProps);
