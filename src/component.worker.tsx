c9d921240651dc390f4472c425e8b314892e02a8
Iaroslav <yar.bezruchko@gmail.com>
2024-10-29 16:16:58 +0200

    fix

--- src/component.worker.tsx
+++ src/component.worker.tsx
@@ -1,16 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { expose } from "react-worker-components";
 import RemoteComponent from "./RemoteComponent";
-import { EventEmitter } from "events";
-
-const eventEmitter = new EventEmitter();
 
 self.onmessage = (event) => {
   console.log(
     "Worker received message (props from main thread): ",
     event.data.props.o
   );
-  eventEmitter.emit("updateProps", event.data.props.o);
 };
 
 const RemoteWorkerComponent = ({
@@ -20,13 +16,6 @@ const RemoteWorkerComponent = ({
   props: any;
   children?: any;
 }) => {
-  const handleUpdateProps = (newProps: any) => {
-    console.log(newProps);
-    expose(RemoteWorkerComponent);
-  };
-
-  eventEmitter.on("updateProps", handleUpdateProps);
-
   return (
     <div>
       <h1>Remote Worker Component</h1>
@@ -38,6 +27,6 @@ const RemoteWorkerComponent = ({
   );
 };
 
-export default RemoteWorkerComponent;
-
 expose(RemoteWorkerComponent);
+
+export default RemoteWorkerComponent;
