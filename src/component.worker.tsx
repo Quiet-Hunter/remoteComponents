ba580a069b4383d6413018b7e2a3a7643abc391a
Iaroslav <yar.bezruchko@gmail.com>
2024-10-29 15:47:07 +0200

    fix

--- src/component.worker.tsx
+++ src/component.worker.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import { expose } from "react-worker-components";
 import RemoteComponent from "./RemoteComponent";
@@ -14,14 +14,13 @@ const RemoteWorkerComponent = ({
   props: any;
   children?: any;
 }) => {
-  const [propsFromMain, _setPropsFromMain] = useState(props);
   return (
     <div>
       <h1>Remote Worker Component</h1>
       <h3>Main TextBox</h3>
       {children}
       <h3>Worker TextBox</h3>
-      <RemoteComponent {...propsFromMain} />
+      <RemoteComponent {...props} />
     </div>
   );
 };
