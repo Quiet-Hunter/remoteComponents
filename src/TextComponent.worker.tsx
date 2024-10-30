df637733e5e4aee493982633ef4caa5dd4f6f50b
Iaroslav <yar.bezruchko@gmail.com>
2024-10-30 14:32:45 +0200

    fix

--- src/TextComponent.worker.tsx
+++ src/TextComponent.worker.tsx
@@ -3,6 +3,10 @@ import React, { ReactNode } from "react";
 import { expose } from "react-worker-components";
 import ChildComponent from "./ChildComponent";
 
+self.onmessage = (event) => {
+  console.log("Remote Worker got message: " + JSON.stringify(event.data));
+};
+
 const TextComponent = ({
   text,
   children,
@@ -14,7 +18,7 @@ const TextComponent = ({
     <div>
       <div>{text}</div>
       {children}
-      <ChildComponent />
+      <ChildComponent {...{ text }} />
     </div>
   );
 };
